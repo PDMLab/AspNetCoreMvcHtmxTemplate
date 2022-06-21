c46dcc983f58c6d3034065cd80870d6b0344980c
Alexander Zeitler <alexander.zeitler@pdmlab.com>
2022-06-22 01:11:42 +0200

    feat: add logging using serilog

--- src/AspNetCoreMvcHtmx/Program.cs
+++ src/AspNetCoreMvcHtmx/Program.cs
@@ -1,4 +1,12 @@
+using AspNetCoreMvcHtmx.Core;
+using Serilog;
+using Serilog.Extensions.Logging;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Host.AddLogging();
+var logger = new SerilogLoggerFactory(Log.Logger)
+  .CreateLogger<Program>();
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
